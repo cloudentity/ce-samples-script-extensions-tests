bc1d0e09639aa78cb7fce423c47a4ff697974b20
Billy <bbray@cloudentity.com>
2022-04-25 15:54:40 -0400

    moved tests to separate files

--- idpdiscovery_header.js
+++ idpdiscovery_header.js
@@ -1,7 +1,7 @@
 module.exports = async function (context) {
     var idpIDs = [];
     for (var i = 0; i < context.idps.length; i++) {
-        if (context.idps[i].name === "SomeIDP" && context.request.headers["Custom-Header"]) {
+        if (context.idps[i].name === "SomeIDP" && context.request.headers["Custom-Header"] && context.request.headers["Custom-Header"][0] === "value1") {
             idpIDs.push(context.idps[i].id);
         }
     }
--- idpdiscovery_query_param.test.js
+++ idpdiscovery_query_param.test.js
@@ -1,6 +1,39 @@
-const idpDiscovery = require('./idpdiscovery');
+const idpDiscoveryQueryParam = require('./idpdiscovery_query_param');
 
-test('idp discovery filters by client id and idp name', () => {
+test('idp discovery filters by query param present', () => {
+
+    let input = {
+        "context": {
+            "client": {
+                "client_id": "default-demo"
+            },
+            "idps": [
+                {
+                    "name": "Default",
+                    "id": "default"
+                },
+                {
+                    "name": "SomeIDP",
+                    "id": "some-idp"
+                }
+            ],
+            request: {
+                "query_params": {
+                    "login_hint":"myuser"
+                }
+            }
+        }
+    }
+
+    var expected = { "idps_ids": ["some-idp"] };
+
+    return idpDiscoveryQueryParam(input.context).then(data => {
+        expect(data).toStrictEqual(expected);
+    });
+})
+
+
+test('idp discovery filters by query param not present', () => {
 
     let input = {
         "context": {
@@ -24,42 +57,14 @@ test('idp discovery filters by client id and idp name', () => {
                       ],
                 },
                 "query_params": {
-                    "login_hint":"myuser"
                 }
             }
         }
     }
 
-    var expected = { "idps_ids": ["default"] };
-
-    return idpDiscovery(input.context).then(data => {
-        expect(data).toStrictEqual(expected);
-    });
-})
-
-test('idp discovery returns empty array for idp name not present', () => {
-
-    let input = {
-        "context": {
-            "client": {
-                "client_id": "default-demo"
-            },
-            "idps": [
-                {
-                    "name": "SomeIDP",
-                    "id": "some-idp"
-                }
-            ],
-        }
-    }
-
     var expected = { "idps_ids": [] };
 
-    return idpDiscovery(input.context).then(data => {
+    return idpDiscoveryQueryParam(input.context).then(data => {
         expect(data).toStrictEqual(expected);
     });
 })
-
-
-
-
